8c2977b9c2a493a1a3b02132d018432e35c6a41e
sapierens <sapierens@gmail.com>
2017-08-13 16:56:42 +0300

    Recompiled.

--- src/utils/json-string-tools.js
+++ src/utils/json-string-tools.js
@@ -2,16 +2,18 @@ var jsonStringEscapeMap = {
     '&': '&amp;',
     '<': '&lt;',
     '>': '&gt;',
-    '\\': '\\\\'
+    '\\': '\\\\',
+    '\'': '&#39;'
 };
 var jsonStringUnescapeMap = {
     '&lt;': '<',
     '&gt;': '>',
-    '&amp;': '&'
+    '&amp;': '&',
+    '&#39;': '\''
 };
 export function jsonStringEscape(str) {
-    return str.replace(/[&\<\>\\]/g, function (e) { return jsonStringEscapeMap[e]; });
+    return str.replace(/[&\<\>\\']/g, function (e) { return jsonStringEscapeMap[e]; });
 }
 export function jsonStringUnescape(str) {
-    return str.replace(/(&amp;)|(&lt;)|(&gt;)/g, function (e) { return jsonStringUnescapeMap[e]; });
+    return str.replace(/(&amp;)|(&lt;)|(&gt;)|(&#39;)/g, function (e) { return jsonStringUnescapeMap[e]; });
 }
